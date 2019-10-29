8d2e7f18a25582d517a703a3495da8d9d82ee958
Mitja Potocin <mitja.potocin@gmail.com>
2019-10-29 13:47:29 +0100

    Added animation to popup menu

--- js/main.js
+++ js/main.js
@@ -3,15 +3,21 @@ const menuButtons = document.getElementsByClassName("menu-item");
 
 document.querySelector("#menu-button").addEventListener("click", () => {
   toggleMenu();
+  TweenLite.to(".menu-items", 0, { x: "15%" });
+  TweenLite.to(popupMenu, 0, { opacity: 1 });
+  TweenLite.from(popupMenu, 0.5, { opacity: 0 });
+  TweenLite.from(".menu-items", 0.9, { ease: Power4.easeOut, x: 300 });
 });
 document.querySelector("#popup-menu-button").addEventListener("click", () => {
-  toggleMenu();
+  TweenLite.to(popupMenu, 0.2, { opacity: 0 });
+  TweenLite.to(".menu-items", 0.2, { x: 300 });
+  TweenLite.delayedCall(0.2, toggleMenu);
 });
 
 function toggleMenu() {
-  popupMenu.style.display = popupMenu.style.display === "none" ? "block" : "none";
+  popupMenu.style.display =
+    popupMenu.style.display === "none" ? "block" : "none";
 }
-
 
 for (i = 0; i < menuButtons.length; i++) {
   menuButtons[i].addEventListener("mouseenter", i => {
