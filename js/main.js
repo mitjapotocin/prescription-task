daeb8225131bb4d1e499ef2e456dc431ba05c39a
Mitja Potocin <mitja.potocin@gmail.com>
2019-10-29 20:34:11 +0100

    Minor style change

--- js/main.js
+++ js/main.js
@@ -2,31 +2,26 @@ const popupMenu = document.getElementById("popup-menu");
 const menuButtons = document.getElementsByClassName("menu-item");
 
 document.querySelector("#menu-button").addEventListener("click", () => {
-  toggleMenu();
+  popupMenu.style.display = "block"
   TweenLite.to(".menu-items", 0, { x: "15%" });
   TweenLite.to(popupMenu, 0, { opacity: 1 });
   TweenLite.from(popupMenu, 0.5, { opacity: 0 });
   TweenLite.from(".menu-items", 0.9, { ease: Power4.easeOut, x: 300 });
 });
 document.querySelector("#popup-menu-button").addEventListener("click", () => {
-  TweenLite.to(popupMenu, 0.2, { opacity: 0 });
-  TweenLite.to(".menu-items", 0.2, { x: 300 });
-  TweenLite.delayedCall(0.2, toggleMenu);
+  TweenLite.to(popupMenu, 0.3, { opacity: 0 });
+  TweenLite.to(".menu-items", 0.3, { x: 300 });
+  TweenLite.delayedCall(0.3, () => popupMenu.style.display = "none");
 });
 
-function toggleMenu() {
-  popupMenu.style.display =
-    popupMenu.style.display === "none" ? "block" : "none";
-}
-
 for (i = 0; i < menuButtons.length; i++) {
-  menuButtons[i].addEventListener("mouseenter", i => {
+  menuButtons[i].addEventListener("mouseenter", e => {
     popupMenu.style.backgroundBlendMode = "screen";
     popupMenu.style.backgroundColor = window
-      .getComputedStyle(i.path[0])
+      .getComputedStyle(e.path[0])
       .getPropertyValue("background-color");
   });
-  menuButtons[i].addEventListener("mouseleave", i => {
+  menuButtons[i].addEventListener("mouseleave", () => {
     popupMenu.style.backgroundBlendMode = "lighten";
     popupMenu.style.backgroundColor = "#0d1a28";
   });
