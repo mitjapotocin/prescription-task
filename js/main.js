83a3738b0b69a37af8afbfabbe04f2f30549d7f5
Mitja Potocin <mitja.potocin@gmail.com>
2019-10-29 06:38:37 +0100

    Created popup-menu

--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
-document.querySelector('#menu-button').addEventListener('click', () => {
-    console.log("click");
-})
+document.querySelector("#menu-button").addEventListener("click", () => {
+  console.log("click");
+});
+
+const popupMenu = document.getElementById("popup-menu");
+const menuButtons = document.getElementsByClassName("menu-item");
+for (i = 0; i < menuButtons.length; i++) {
+  menuButtons[i].addEventListener("mouseenter", i => {
+    console.log(i);
+    popupMenu.style.backgroundColor = window
+      .getComputedStyle(i.path[0])
+      .getPropertyValue("background-color");
+  });
+  menuButtons[i].addEventListener("mouseleave", i => {
+    popupMenu.style.backgroundColor = "#0d1a28";
+  });
+}
