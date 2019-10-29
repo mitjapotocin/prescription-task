6a1f9ebb441524527759c134bbabe0c893756ddc
Mitja Potocin <mitja.potocin@gmail.com>
2019-10-29 12:47:17 +0100

    Finished popu menu

--- js/main.js
+++ js/main.js
@@ -1,17 +1,27 @@
-document.querySelector("#menu-button").addEventListener("click", () => {
-  console.log("click");
-});
-
 const popupMenu = document.getElementById("popup-menu");
 const menuButtons = document.getElementsByClassName("menu-item");
+
+document.querySelector("#menu-button").addEventListener("click", () => {
+  toggleMenu();
+});
+document.querySelector("#popup-menu-button").addEventListener("click", () => {
+  toggleMenu();
+});
+
+function toggleMenu() {
+  popupMenu.style.display = popupMenu.style.display === "none" ? "block" : "none";
+}
+
+
 for (i = 0; i < menuButtons.length; i++) {
   menuButtons[i].addEventListener("mouseenter", i => {
-    console.log(i);
+    popupMenu.style.backgroundBlendMode = "screen";
     popupMenu.style.backgroundColor = window
       .getComputedStyle(i.path[0])
       .getPropertyValue("background-color");
   });
   menuButtons[i].addEventListener("mouseleave", i => {
+    popupMenu.style.backgroundBlendMode = "lighten";
     popupMenu.style.backgroundColor = "#0d1a28";
   });
 }
