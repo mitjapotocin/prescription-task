a89a507b2c1d2426e35dfe26677cc18e1a4fa2d3
Mitja Potocin <mitja.potocin@gmail.com>
2019-10-30 06:47:12 +0100

    Escape key hides popup menu

--- dist/main.js
+++ dist/main.js
@@ -2,17 +2,26 @@ const popupMenu = document.getElementById("popup-menu");
 const menuButtons = document.getElementsByClassName("menu-item");
 
 document.querySelector("#menu-button").addEventListener("click", () => {
-  popupMenu.style.display = "block"
+  popupMenu.style.display = "block";
   TweenLite.to(".menu-items", 0, { x: "15%" });
   TweenLite.to(popupMenu, 0, { opacity: 1 });
   TweenLite.from(popupMenu, 0.5, { opacity: 0 });
   TweenLite.from(".menu-items", 0.9, { ease: Power4.easeOut, x: 300 });
 });
 document.querySelector("#popup-menu-button").addEventListener("click", () => {
+  closePopupMenu();
+});
+
+document.addEventListener("keydown", e => {
+  if (e.key === "Escape") {
+    closePopupMenu();
+  }
+});
+function closePopupMenu() {
   TweenLite.to(popupMenu, 0.3, { opacity: 0 });
   TweenLite.to(".menu-items", 0.3, { x: 300 });
-  TweenLite.delayedCall(0.3, () => popupMenu.style.display = "none");
-});
+  TweenLite.delayedCall(0.3, () => (popupMenu.style.display = "none"));
+}
 
 for (i = 0; i < menuButtons.length; i++) {
   menuButtons[i].addEventListener("mouseenter", e => {
